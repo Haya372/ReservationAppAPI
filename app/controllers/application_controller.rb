83ec92af593bb70b132ee30065fac6fb52ae3f11
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-24 22:02:45 +0900

    [fix] user API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
   class UnAuthorizationError < StandardError; end
+  class ForbiddenError < StandardError; end
   rescue_from UnAuthorizationError, with: :handle_un_authorization_err
   rescue_from JwtAuth::TokenExpiredError, with: :handle_token_expired
   rescue_from JWT::VerificationError, with: :handle_un_authorization_err
+  rescue_from ForbiddenError, with: :handle_forbidden_error
 
   def handle_un_authorization_err
     logger.error("authorization error")
@@ -12,5 +14,10 @@ class ApplicationController < ActionController::API
   def handle_token_expired
     logger.error("token expired")
     render json: "token expired", status: :unauthorized
+  end
+
+  def handle_forbidden_error
+    logger.error("forbidden")
+    render json: "Forbidden", status: :forbidden
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :roles, through: :user_roles
-  has_many :user_roles
+  has_many :user_roles, dependent: :delete_all
   has_many :organizations, through: :user_organizations
-  has_many :user_organizations
+  has_many :user_organizations, dependent: :delete_all
   has_many :reservations, through: :user_reservations
-  has_many :user_reservations
+  has_many :user_reservations, dependent: :delete_all
 end
