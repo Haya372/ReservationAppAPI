4d91ed52a597c3052456f352bcd51e12425d7c81
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-25 00:22:09 +0900

    [add] 場所追加API

---
+++ app/controllers/api/spaces_controller.rb
@@ -0,0 +1,43 @@
+class Api::SpacesController < ApplicationController
+  include JwtAuth
+  before_action :jwt_authenticate
+
+
+  def create
+    space = Space.create(space_params)
+    space.organizations << Organization.find(permitted_organization_id[:organization_id])
+    render json: space
+  end
+
+  def show
+    render json: Space.find(params[:id])
+  end
+
+  def index
+    render json: Space.belong_organization(permitted_organization_id[:organization_id])
+  end
+
+  def update
+    space = Space.find(params[:id])
+    space.update(space_params)
+    render json: space
+  end
+
+  def destroy
+    Space.find(params[:id]).destroy
+    render json: "success"
+  end
+
+  def check_perm
+    # 団体編集権限があるかどうか
+    # role APIが完成後に処理を追記
+  end
+
+  def space_params
+    params.permit(:name, :capacity)
+  end
+
+  def permitted_organization_id
+    params.permit(:organization_id)
+  end
+end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,9 @@
 class Space < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :delete_all
+  has_many :organization_spaces, dependent: :delete_all
   has_many :organizations, through: :organization_spaces
-  has_many :organization_spaces
+
+  scope :belong_organization, -> organization_id {
+    joins(:organizations).where('organizations.id = ?', organization_id)
+  }
 end
