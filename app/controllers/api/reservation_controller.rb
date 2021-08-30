4067af63e9499dafb924363c0e64ad58c3a967a1
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-30 18:14:22 +0900

    [fix] sumOnlyがnullの時のバグ修正

--- app/controllers/api/reservation_controller.rb
+++ app/controllers/api/reservation_controller.rb
@@ -25,7 +25,7 @@ class Api::ReservationController < ApplicationController
   def index
     if !params[:start_time].blank? && !params[:end_time].blank?
       raise BadRequestError if DateTime.parse(params[:end_time]) - DateTime.parse(params[:start_time]) <= 0
-      if params[:sumOnly].downcase == "true"
+      if params[:sumOnly] && params[:sumOnly].downcase == "true"
         common = Reservation.common_part(permitted_space_id[:space_id], params.permit(:start_time)[:start_time], params.permit(:end_time)[:end_time])
         render json: { "sum" => common.sum(:numbers) }
       else
