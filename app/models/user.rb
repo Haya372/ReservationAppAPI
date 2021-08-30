63f43c0a5a11cbf7f00e88315f2a836ae1122ae1
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-31 00:19:40 +0900

    [add] user apiのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: true, on: :update
+  validates :password, presence: true, on: :update
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }}
+
   has_many :roles, through: :user_roles
   has_many :user_roles, dependent: :delete_all
   has_many :organizations, through: :user_organizations
