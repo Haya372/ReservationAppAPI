f9f77bffb6f63f466f44e704705dd80613385eb8
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-31 00:48:58 +0900

    [add] organization apiのバリデーション

--- app/controllers/api/organization_controller.rb
+++ app/controllers/api/organization_controller.rb
@@ -4,21 +4,32 @@ class Api::OrganizationController < ApplicationController
   before_action :check_perm, only: [:destroy, :update]
 
   def create
-    organization = Organization.new(organization_params)
-    organization.users << @current_user
-    organization.user_roles << UserRole.new(user_id: @current_user.id, role_id: 1)
-    raise BadRequestError if !organization.save
-    render json: organization
+    ActiveRecord::Base.transaction do
+      begin
+        @organization = Organization.create(organization_params)
+        @organization.users << @current_user
+        @organization.user_roles << UserRole.new(user_id: @current_user.id, role_id: 1)
+        @organization.save!
+      rescue ActiveRecord::RecordInvalid, ActiveRecord::NotNullViolation => e
+        raise BadRequestError
+      end
+    end
+    render json: @organization
   end
 
   def update
-    organization = Organization.find(params[:id].to_i)
-    organization.update(organization_params)
+    organization = Organization.find(params[:id])
+    begin
+      organization.update!(organization_params)
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::NotNullViolation
+      raise BadRequestError
+    end
     render json: organization
   end
 
   def show
     organization = Organization.find(params[:id])
+    raise ActiveRecord::RecordNotFound if organization.blank?
     render json: {
       "id" => organization.id,
       "name" => organization.name,
