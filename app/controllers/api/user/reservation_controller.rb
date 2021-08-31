31ab4464d2f3c28cb026bc769d9b70ef808da072
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-08-31 23:47:37 +0900

    [add]current_userの予約一覧を返すAPI

---
+++ app/controllers/api/user/reservation_controller.rb
@@ -0,0 +1,8 @@
+class Api::User::ReservationController < ApplicationController
+  include JwtAuth
+  before_action :jwt_authenticate
+
+  def index
+    render json: Reservation.where(user_id: @current_user.id)
+  end
+end
