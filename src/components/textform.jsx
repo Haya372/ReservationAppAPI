44d4b28e8a139bb95f77dda2b7ab9269901882cb
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-08 19:10:04 +0900

    [add] textform component

---
+++ src/components/textform.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import TextField from '@material-ui/core/TextField';
+import Alert from '@material-ui/lab/Alert';
+
+export default function TextForm(props){
+  const [err, setErr] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  const onChange = (e) => {
+    if(props.notNull){
+      setErr(e.target.value.length == 0);
+      if(e.target.value.length == 0){
+        setErrMsg(`${props.label}は入力必須です`);
+      }
+    }
+    props.onChange(e);
+  }
+
+  return (
+    <div>
+      <TextField
+        label={props.label}
+        value={props.value}
+        onChange={onChange}
+        fullWidth={props.fullWidth}
+        variant={props.variant}
+        type={props.type}
+        error={err}
+      />
+      { err ?
+        <Alert severity="error">{errMsg}</Alert>
+        : null
+      }
+    </div>
+  )
+}
