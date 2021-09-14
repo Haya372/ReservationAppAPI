2cd612479852ce5b98eea0e6168447463ba21fe9
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-14 23:36:43 +0900

    [fix] heroku proccess

--- entrypoint.prd.sh
+++ entrypoint.prd.sh
@@ -6,22 +6,7 @@ rm -f /myapp/tmp/pids/server.pid
 
 cd /myapp
 
-if [[ -e ./src ]]; then
-  # on first build
-  echo "run npm install"
-  npm install
-
-  echo "run npm upgrade --force"
-  # npm update --force
-
-  echo "npm run build"
-  npm run build
-
-  echo "remove node files"
-  rm -rf ./src
-
-  rails db:migrate
-fi
+rails db:migrate
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
