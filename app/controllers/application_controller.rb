56cf0f2f1a764da5bbbb27449f9318db2edc330b
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-17 23:49:28 +0900

    [fix] authの500エラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,28 +4,29 @@ class ApplicationController < ActionController::API
   class BadRequestError < StandardError; end
   rescue_from UnAuthorizationError, with: :handle_un_authorization_err
   rescue_from JwtAuth::TokenExpiredError, with: :handle_token_expired
+  rescue_from JWT::DecodeError, with: :handle_un_authorization_err
   rescue_from JWT::VerificationError, with: :handle_un_authorization_err
   rescue_from ForbiddenError, with: :handle_forbidden_error
   rescue_from BadRequestError, with: :handle_badrequest_error
   rescue_from ActiveModel::StrictValidationFailed, with: :handle_validation_error
 
-  def handle_un_authorization_err
-    logger.error("authorization error")
+  def handle_un_authorization_err(e)
+    logger.debug e
     render json: "AuthorizationError", status: :unauthorized
   end
 
-  def handle_token_expired
-    logger.error("token expired")
+  def handle_token_expired(e)
+    logger.debug e
     render json: "token expired", status: :unauthorized
   end
 
-  def handle_forbidden_error
-    logger.error("forbidden")
+  def handle_forbidden_error(e)
+    logger.debug e
     render json: "Forbidden", status: :forbidden
   end
 
-  def handle_badrequest_error
-    logger.error('bad request')
+  def handle_badrequest_error(e)
+    logger.debug e
     render json: "Bad Request", status: :bad_request
   end
 
