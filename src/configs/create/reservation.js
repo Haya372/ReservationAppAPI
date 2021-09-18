6dabb08191ba230ec33fd86c7c4b1b4ea58e385e
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-18 16:20:31 +0900

    [add] 管理者権限の予約機能

--- src/configs/create/reservation.js
+++ src/configs/create/reservation.js
@@ -1,46 +1,4 @@
 export default {
-  space_name: {
-    size: 12,
-    required: true,
-    component: 'text',
-    props: {
-      fullWidth: true,
-      notNull: true,
-      label: "場所名"
-    }
-  },
-  numbers: {
-    size: 4,
-    required: true,
-    component: 'number',
-    props: {
-      label: '人数',
-      fullWidth: true
-    }
-  },
-  user_id: {
-    size: 2,
-    required: true,
-    component: 'text',
-    props: {
-      label: '予約者',
-    }
-  },
-  users: {
-    size: 4,
-    component: 'text',
-    props: {
-      label: '参加者',
-    }
-  },
-  admin_flg: {
-    size: 2,
-    component: 'flg',
-    props: {
-      label: "管理者予約",
-    },
-    type: 'boolean'
-  },
   memo: {
     size: 12,
     component: 'textmulti',
